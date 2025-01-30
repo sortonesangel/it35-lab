6189fddb415e65816d7af6a1ce6d1729dda721ea
Angel <angle@gmail.com>
2025-01-30 14:20:59 +0800

    add it35-lab

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'it35-lab',
+  webDir: 'dist'
+};
+
+export default config;
