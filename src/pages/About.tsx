578caf1e71c014b03c7f145d7848fd0fd5f66526
angel <angelsortones@gmail.com>
2025-02-12 16:14:48 +0800

    added feature/about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -8,7 +8,7 @@ import {
       IonToolbar 
   } from '@ionic/react';
   
-  const Template: React.FC = () => {
+  const About: React.FC = () => {
     return (
       <IonPage>
         <IonHeader>
@@ -16,7 +16,7 @@ import {
             <IonButtons slot='start'>
               <IonMenuButton></IonMenuButton>
             </IonButtons>
-            <IonTitle>Template</IonTitle>
+            <IonTitle>About</IonTitle>
           </IonToolbar>
         </IonHeader>
         <IonContent fullscreen>
@@ -25,4 +25,4 @@ import {
     );
   };
   
-  export default Template;
+  export default About;
