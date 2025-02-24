1420a5812d75c04f2877e9dadf682585ed96d0df
angel <sortonesangel@gmail.com>
2025-02-24 19:05:10 +0800

    Added Login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,37 +1,78 @@
 import { 
-  IonButton,
+  IonAvatar,
+    IonButton,
     IonButtons,
+      IonCol,
       IonContent, 
+      IonGrid, 
       IonHeader, 
+      IonInput, 
+      IonInputPasswordToggle, 
       IonMenuButton, 
       IonPage, 
+      IonRow, 
       IonTitle, 
       IonToolbar, 
       useIonRouter
   } from '@ionic/react';
-  
+
   const Login: React.FC = () => {
-      const navigation = useIonRouter(); 
+    const navigation = useIonRouter();
 
-      const doLogin = () => {
+    const doLogin = () => {
         navigation.push('/it35-lab/app','forward','replace');
-      }
-        
-
+    }
     return (
       <IonPage>
         <IonHeader>
-          <IonToolbar>            
+          <IonToolbar>
             <IonTitle>Login</IonTitle>
           </IonToolbar>
         </IonHeader>
         <IonContent className='ion-padding'>
-        <IonButton onClick={() => doLogin()} expand="full">
-          Login
-        </IonButton>
+        
+        <div
+          style={{
+            marginTop: '25%',
+          }}
+        >
+          <IonGrid style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+          }}>
+            <IonRow >
+              
+              <IonCol size="8">
+                <IonAvatar style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}>
+                  <img 
+                  style={{
+                    width:'100px',
+                    objectFit:'cover',
+                  }}
+                  alt="Silhouette of a person's head" src="https://assets.florista.ph/uploads/product-pics/2486_58_2486.webp" />
+                </IonAvatar>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+          <IonInput label="Username" >
+          </IonInput>
+          <IonInput type="password" label="Password" value="Stephanie">
+            <IonInputPasswordToggle  slot="end"></IonInputPasswordToggle>
+          </IonInput>
+          <IonButton onClick={() => doLogin()} expand="full">
+            Login
+          </IonButton>
+        </div>
+            
         </IonContent>
       </IonPage>
     );
   };
-  
-  export default Login;
+
+export default Login;
