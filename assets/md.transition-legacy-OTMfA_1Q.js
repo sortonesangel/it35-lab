0a4cc269fb32a14bff64cfbecbb0b0d63c8c2202
Angel <sortonesangel@gmail.com>
2025-04-07 17:01:10 +0800

    Updates

--- assets/md.transition-legacy-OTMfA_1Q.js
+++ assets/md.transition-legacy-OTMfA_1Q.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DVVHj9pZ.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-CJqP00Al.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
