2c34380331a5ceee8fa0b1c53608cb763504d1b7
angel <sortonesangel@gmail.com>
2025-04-10 13:32:12 +0800

    Updates

--- assets/ios.transition-legacy-CNA-tH8D.js
+++ assets/ios.transition-legacy-CNA-tH8D.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CJqP00Al.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-B78z4ud7.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-Bvu4gxQz.js
+++ assets/swipe-back-legacy-Bvu4gxQz.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CJqP00Al.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-B78z4ud7.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
