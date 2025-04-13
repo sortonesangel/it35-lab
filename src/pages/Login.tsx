c2468a710ff6c7b113bee289400fabb825c20d8e
Angel <sortonesangel>
2025-04-13 18:40:36 +0800

    added utils

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -12,7 +12,7 @@ import {
 } from '@ionic/react';
 import { logoIonic } from 'ionicons/icons';
 import { useState } from 'react';
-import { supabase } from '../utils/supabaseClients';
+import { supabase } from '../utils/supabaseClient';
 
 const AlertBox: React.FC<{ message: string; isOpen: boolean; onClose: () => void }> = ({ message, isOpen, onClose }) => {
   return (
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -15,7 +15,7 @@ import {
     IonCardTitle,
     IonAlert,
 } from '@ionic/react';
-import { supabase } from '../utils/supabaseClients';
+import { supabase } from '../utils/supabaseClient';
 import bcrypt from 'bcryptjs';
 
 // Reusable Alert Component
