9ab280e2d9ab6f97f1004f128badfe2f2123d259
Angel <sortonesangel>
2025-04-13 18:37:13 +0800

    added register

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -15,7 +15,7 @@ import {
     IonCardTitle,
     IonAlert,
 } from '@ionic/react';
-import { supabase } from '../utils/supabaseClient';
+import { supabase } from '../utils/supabaseClients';
 import bcrypt from 'bcryptjs';
 
 // Reusable Alert Component
