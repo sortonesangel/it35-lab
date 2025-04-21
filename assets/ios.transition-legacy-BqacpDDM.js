d06af1b745906f8215a8e512bc325e9e8e7d8595
Angel <sortonesangel>
2025-04-21 23:16:34 +0800

    Updates

--- assets/ios.transition-legacy-BqacpDDM.js
+++ assets/ios.transition-legacy-BqacpDDM.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-4NzuY8w_.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-rPrZNneO.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
