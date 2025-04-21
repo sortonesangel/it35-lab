8359946dfece9271ef28e2b1641261588001b024
angel <sortonesangel@gmail.com>
2025-04-21 14:28:04 +0800

    Updates

--- assets/md.transition-legacy-BQ_Rql0H.js
+++ assets/md.transition-legacy-BQ_Rql0H.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-B78z4ud7.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-4NzuY8w_.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
