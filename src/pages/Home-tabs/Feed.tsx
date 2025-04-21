e6ac13ce3ea56ffc43f9ed3070f6533a0a200862
Angel <sortonesangel>
2025-04-21 23:56:31 +0800

    added Feed

--- src/pages/Home-tabs/Feed.tsx
+++ src/pages/Home-tabs/Feed.tsx
@@ -1,10 +1,5 @@
 import { 
   IonButtons,
-    IonCard,
-    IonCardContent,
-    IonCardHeader,
-    IonCardSubtitle,
-    IonCardTitle,
     IonContent, 
     IonHeader, 
     IonMenuButton, 
@@ -12,27 +7,12 @@ import {
     IonTitle, 
     IonToolbar 
 } from '@ionic/react';
+import FeedContainer from '../../components/FeedContaniner';
 
 const Feed: React.FC = () => {
-  const feed = [
-   
-      
-    ];
-    
-
-  
   return (
     <IonPage>
       <IonHeader>
-      <IonCard>
-      <img alt="Silhouette of mountains" src="https://www.usatoday.com/gcdn/-mm-/7fbb4dcd0e29b46245bb2ec4981ee02ad9e486c3/c=518-0-3419-1632/local/-/media/2016/01/24/USATODAY/usatsports/d1flagv2_toothless-jy-5084.jpg" />
-      <IonCardHeader>
-        <IonCardTitle>Toothless</IonCardTitle>
-        <IonCardSubtitle>Night Fury</IonCardSubtitle>
-      </IonCardHeader>
-
-      <IonCardContent>My Favorite Character</IonCardContent>
-    </IonCard>
         <IonToolbar>
           <IonButtons slot='start'>
             <IonMenuButton></IonMenuButton>
@@ -41,16 +21,7 @@ const Feed: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100%',
-          }}
-        >
-          Feed
-        </div>
+        <FeedContainer />
       </IonContent>
     </IonPage>
   );
