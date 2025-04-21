995230d033736ee1a7032437b1286694a4619aac
Angel <sortonesangel>
2025-04-22 07:54:05 +0800

    Updates

--- assets/status-tap-legacy-C8u1EqeQ.js
+++ assets/status-tap-legacy-C8u1EqeQ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C3vt15J7.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-C50jnwCE.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
