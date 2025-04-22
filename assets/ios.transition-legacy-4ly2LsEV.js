6638186f2b10cff051ec33a9a34299cb49731a44
Angel <sortonesangel>
2025-04-22 08:05:30 +0800

    Updates

--- assets/ios.transition-legacy-4ly2LsEV.js
+++ assets/ios.transition-legacy-4ly2LsEV.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C50jnwCE.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-C3vt15J7.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
