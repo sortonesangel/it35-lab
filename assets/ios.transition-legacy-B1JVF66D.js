654ff5171356058012bb3ae4c01e1db4967aabfa
Angel <sortonesangel>
2025-04-27 20:52:15 +0800

    Updates

--- assets/ios.transition-legacy-B1JVF66D.js
+++ assets/ios.transition-legacy-B1JVF66D.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CHRqrK_y.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BSu_FuEA.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-3bfNvmP4.js
+++ assets/md.transition-legacy-3bfNvmP4.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CHRqrK_y.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-BSu_FuEA.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
