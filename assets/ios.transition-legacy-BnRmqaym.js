c01571a476035951bb3708280c5aec3349908aef
angel <sortonesangel@gmail.com>
2025-04-28 14:12:08 +0800

    Updates

--- assets/ios.transition-legacy-BnRmqaym.js
+++ assets/ios.transition-legacy-BnRmqaym.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BSu_FuEA.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DizpSzX4.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
