0f5ef7794f422b87190600bdbf663133ec4d4b96
angel <sortonesangel@gmail.com>
2025-04-29 15:06:39 +0800

    added search

--- src/pages/Home-tabs/Search.tsx
+++ src/pages/Home-tabs/Search.tsx
@@ -1,36 +1,46 @@
-import { 
+import {
   IonButtons,
-    IonContent, 
-    IonHeader, 
-    IonMenuButton, 
-    IonPage, 
-    IonTitle, 
-    IonToolbar 
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonSearchbar,
 } from '@ionic/react';
 
 const Search: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonButtons slot='start'>
-            <IonMenuButton></IonMenuButton>
+        <IonToolbar color="primary">
+          <IonButtons slot="start">
+            <IonMenuButton />
           </IonButtons>
           <IonTitle>Search</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
+
+      <IonContent fullscreen className="ion-padding" style={{ 
+        background: 'linear-gradient(to bottom right, #f3f4f6, #e5e7eb)' 
+      }}>
+        <IonSearchbar
+          placeholder="Search for items..."
+          debounce={300}
+          animated
+        ></IonSearchbar>
+
         <div
           style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100%',
+            marginTop: '40px',
+            textAlign: 'center',
+            color: '#4B5563',
+            fontSize: '1.5rem',
+            fontWeight: '500',
           }}
         >
-          Search
+          Start typing to search.
         </div>
-
       </IonContent>
     </IonPage>
   );
