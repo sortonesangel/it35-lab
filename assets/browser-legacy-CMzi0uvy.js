503336b9da317b42abf1d1b6fd5c770c9888a529
angel <sortonesangel@gmail.com>
2025-04-29 14:05:59 +0800

    Updates

--- assets/browser-legacy-CMzi0uvy.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-DizpSzX4.js"],(function(e,t){"use strict";var r;return{setters:[e=>{r=e.q}],execute:function(){const t=r((function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")})),o=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"}));e("b",o)}}}));
